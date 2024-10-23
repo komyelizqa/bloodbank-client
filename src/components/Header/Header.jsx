4ff44776d45aa73ff7dcaf8df2b89c621c173d18
komyelizqa <yelizavkom@gmail.com>
2024-10-22 21:18:50 -0700

    add structure to the project, add routes, add global style

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./Header.scss";
+import logo from "../../assets/logo/BrainFlix-logo.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import avatar2 from "../../assets/images/second_avatar.jpg";
+import { Link } from "react-router-dom";
+// import Button from '../Button/Button';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        {/* <Link to="/" className="header__nav-link"> */}
+          {/* <img className="header__logo" src={logo} alt="BloodBank Logo" /> */}
+        {/* </Link> */}
+        {/* <Link to="/upload" className="header__btn-link"> */}
+          {/* <Button className="header__btn">
+            UPLOAD
+          </Button> */}
+        {/* </Link> */}
+        {/* <img
+          src={avatar}
+          className="header__avatar header__avatar--main"
+          alt="avatar"
+        />
+        <img
+          src={avatar2}
+          className="header__avatar header__avatar--secondary"
+          alt="avatar-secondary"
+        /> */}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./HomePage.scss";
+
+function HomePage() {
+  return (
+    <>
+      <main></main>
+    </>
+  );
+}
+
+export default HomePage;
