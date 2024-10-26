88d56ab3398900daead6b1b27d6909bc6e9ff9b8
komyelizqa <yelizavkom@gmail.com>
2024-10-26 01:19:16 -0700

    add Header structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo/bloodbank-logo.png";
 // import Button from '../Button/Button';
 
 function Header() {
   return (
     <header className="header">
       <div className="header__container">
-        {/* <Link to="/" className="header__nav-link"> */}
-          {/* <img className="header__logo" src={logo} alt="BloodBank Logo" /> */}
-        {/* </Link> */}
+        <Link to="/" className="header__nav-link">
+          <img className="header__logo" src={logo} alt="BloodBank Logo" />
+        </Link>
         {/* <Link to="/upload" className="header__btn-link"> */}
           {/* <Button className="header__btn">
             UPLOAD
           </Button> */}
         {/* </Link> */}
+        <h4>Volunteer</h4>
         {/* <img
           src={avatar}
           className="header__avatar header__avatar--main"
           alt="avatar"
-        />
-        <img
-          src={avatar2}
-          className="header__avatar header__avatar--secondary"
-          alt="avatar-secondary"
         /> */}
       </div>
     </header>
